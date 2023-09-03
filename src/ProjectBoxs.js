7a8f53e708fad4f50983fd4f4126633a09a9bd50
Lokesh Garg <lokeshgargadv@gmail.com>
2023-09-04 00:57:28 +0530

    ProjectBoxs

--- src/ProjectBoxs.js
+++ src/ProjectBoxs.js
@@ -5,46 +5,44 @@ export default class ProjectBoxs {
     for (let e of this.projectBoxs) {
       e.addEventListener("mousemove", (ev) => this.onMouseMove(ev, e));
       e.addEventListener("mouseover", (ev) => this.onMouseOver(ev));
-      e.addEventListener("mouseout", (ev) => this.onMouseOut(ev));
+      e.addEventListener("mouseout", (ev) => this.onMouseOut(ev, e));
     }
   }
 
   onMouseMove(event, e) {
-    console.log(
-      event.offsetX,
-      event.offsetY,
-      event.clientX,
-      event.clientY,
-      event.movementX,
-      event.movementY,
-      event.target.offsetLeft,
-      event.target.offsetTop,
-      event.target.offsetWidth,
-      event.target.offsetHeight,
-      event.offsetX / event.target.offsetWidth,
-      event.offsetY / event.target.offsetHeight,
-      event
-    );
-
     let offsetLeft = 0;
     let offsetTop = 0;
-    let test = e;
-    while (test != document.body) {
-      offsetLeft += test.offsetLeft;
-      offsetTop += test.offsetTop;
-      //   console.log(test, test.offsetLeft, test.offsetTop, offsetLeft, offsetTop);
-      test = test.parentElement;
+    let t = e;
+    while (t != document.body) {
+      offsetLeft += t.offsetLeft;
+      offsetTop += t.offsetTop;
+      t = t.parentElement;
     }
-    console.log(offsetLeft, offsetTop);
-    // console.log(
-    //   (event.offsetX / event.target.offsetWidth) * 15,
-    //   (event.offsetY / event.target.offsetHeight) * 15
-    // );
+
+    let x =
+      ((offsetLeft + e.offsetWidth / 2 - event.pageX) / (e.offsetWidth / 2)) *
+      17;
+    let y =
+      ((offsetTop + e.offsetHeight / 2 - event.pageY) / (e.offsetHeight / 2)) *
+      17;
+
+    if (x <= 15 && x >= -15 && y <= 15 && y >= -15) {
+      e.style.transform = `perspective(700px) rotateX(${-y}deg) rotateY(${x}deg) scale3d(1.05, 1.05, 1.05)`;
+      e.style.transition = ``;
+    } else {
+      e.style.transform = `perspective(700px) rotateX(${
+        y >= 15 ? -15 : y <= -15 ? 15 : -y
+      }deg) rotateY(${
+        x >= 15 ? 15 : x <= -15 ? -15 : x
+      }deg) scale3d(1.05, 1.05, 1.05)`;
+      e.style.transition = `transform 200ms ease-in`;
+    }
   }
-  onMouseOver(event, e) {
+  onMouseOver(event) {
     window.cursor.cursorSize(2.7);
   }
   onMouseOut(event, e) {
+    e.style.transform = `perspective(700px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`;
     window.cursor.cursorSize(1);
   }
 }
